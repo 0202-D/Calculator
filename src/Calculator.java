76a2ff25575d33b9b763cca0428c8237f64ab222
Dmitriy Petrov <nevermind1979@mail.ru>
2022-06-29 19:17:08 +0300

    Add project

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,6 +1,36 @@
+import java.util.function.*;
+
 /**
  * @author Dm.Petrov
  * DATE: 29.06.2022
  */
 public class Calculator {
+    static Supplier<Calculator> instance = Calculator::new;
+    BinaryOperator<Integer> plus = (x, y) -> x + y;
+    BinaryOperator<Integer> minus = (x, y) -> x - y;
+    BinaryOperator<Integer> multiply = (x, y) -> x * y;
+    // Здесь была возможна ошибка приделении но ноль. Так же можно было бы завернуть этот блок в try catch
+    BinaryOperator<Integer> devide = (x, y) -> {
+        if (y == 0) {
+            return -666666666;
+        } else {
+            return x / y;
+        }
+    };
+    //Вот такой вариант возможен
+    BinaryOperator<Double> devideDouble = (x, y) -> {
+        if (y == 0) {
+            return Double.POSITIVE_INFINITY;
+        } else {
+            return x / y;
+        }
+    };
+    UnaryOperator<Integer> pow = x -> x * x;
+
+    UnaryOperator<Integer> abs = x ->
+            x > 0 ? x : x * -1;
+
+    Predicate<Integer> isPositive = x -> x > 0;
+    Consumer<Double> printlnDouble = System.out::println;
+    Consumer<Integer> println = System.out::println;
 }
--- src/Main.java
+++ src/Main.java
@@ -3,4 +3,18 @@
  * DATE: 29.06.2022
  */
 public class Main {
+    public static void main(String[] args) {
+        Calculator calc = Calculator.instance.get();
+        int a = calc.plus.apply(1, 2);
+        int b = calc.minus.apply(1, 1);
+        int cc = calc.devide.apply(a, b);
+        calc.println.accept(cc);
+        double c = calc.devideDouble.apply((double) a, (double) b);
+        calc.printlnDouble.accept(c);
+        int d = calc.abs.apply(-5);
+        calc.println.accept(d);
+        int e = calc.pow.apply(4);
+        calc.println.accept(e);
+        System.out.println(calc.isPositive.test(-3));
+    }
 }
